a9ca22d6401e135c1059d33524d246c9b0e23581
takuya_kajiwara <fd.elt.119@gmail.com>
2025-05-23 15:44:35 +0900

    supabase及びprisma導入、データフェッチから画面表示まで

--- features/bbs/components/BbsCardList.tsx
+++ features/bbs/components/BbsCardList.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 import * as Bbs from "@/features/bbs/components";
+import { BbsData } from "@/app/types/types";
 
-const BbsCardList = () => {
+interface BbsAllDataProps {
+  bbsAllData: BbsData[];
+}
+
+const BbsCardList = ({ bbsAllData }: BbsAllDataProps) => {
   return (
-    <div className="grid lg:grid-cols-3 px-4 py-4 gap-4">
-      <Bbs.BbsCard />
-      <Bbs.BbsCard />
-      <Bbs.BbsCard />
-      <Bbs.BbsCard />
-      <Bbs.BbsCard />
-      <Bbs.BbsCard />
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-4 py-4 gap-4">
+      {bbsAllData.map((bbsData: BbsData) => {
+        return <Bbs.BbsCard key={bbsData.id} bbsData={bbsData} />;
+      })}
     </div>
   );
 };
