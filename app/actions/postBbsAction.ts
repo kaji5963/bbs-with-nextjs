cfe7e59ddb4fce04942d3ee7dcc45f1e9b0c284f
takuya_kajiwara <fd.elt.119@gmail.com>
2025-05-24 09:30:58 +0900

    一部リファクタリング

--- app/actions/postBbsAction.ts
+++ app/actions/postBbsAction.ts
@@ -4,23 +4,7 @@ import { z } from "zod";
 import { db } from "../../lib/prismaClient";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "ユーザー名は2文字以上で入力してください。",
-  }),
-  title: z.string().min(2, {
-    message: "タイトルは2文字以上で入力してください。",
-  }),
-  content: z
-    .string()
-    .min(10, {
-      message: "本文は10文字以上で入力してください。",
-    })
-    .max(140, {
-      message: "本文は140文字以内で入力してください。",
-    }),
-});
+import { formSchema } from "../schema/formSchema";
 
 export const postBbs = async ({
   username,
--- app/bbs-posts/create/page.tsx
+++ app/bbs-posts/create/page.tsx
@@ -18,23 +18,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { Textarea } from "@/components/ui/textarea";
 import { postBbs } from "@/app/actions/postBbsAction";
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "ユーザー名は2文字以上で入力してください。",
-  }),
-  title: z.string().min(2, {
-    message: "タイトルは2文字以上で入力してください。",
-  }),
-  content: z
-    .string()
-    .min(10, {
-      message: "本文は10文字以上で入力してください。",
-    })
-    .max(140, {
-      message: "本文は140文字以内で入力してください。",
-    }),
-});
+import { formSchema } from "@/app/schema/formSchema";
 
 const CreateBbsPage = () => {
   const form = useForm({
